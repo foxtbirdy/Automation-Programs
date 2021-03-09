13082caaeec1e21c3d64dc47da340b9dfdeb9042
Climax_ <72243209+Code-Blender-7@users.noreply.github.com>
2021-03-09 11:53:10 +0600

    test projects

---
+++ Twitter Sign-in [AUTO]/test.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+user_name = input("Phone, email, username ? > ")
+user_password = input("Password ? > ")
+
+PATH = "D:\\Coding Nerds\\RFC\\edgedriver\\msedgedriver.exe"
+driver = webdriver.Edge(PATH)
+driver.get("http://www.twitter.com/login")
+
+time.sleep(3)
+
+elem_username = driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div[2]/form/div/div[1]/label')
+elem_username.send_keys(user_name) 
+time.sleep(3)
+elem_password = driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div[2]/form/div/div[2]/label')
+elem_password.send_keys(user_password)
+
+time.sleep(3)
+
+elem_clicker = driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div[2]/form/div/div[3]/div/div/span')
+elem_clicker.click()
